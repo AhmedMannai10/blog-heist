836a5893cd3d05b4828f057c5b2e913bf0ac37e3
Ahmed Mannai <mannaiahmed7518@gmail.com>
2022-10-09 21:36:20 +0100

    Fetching usernames from firebase and adding the ability to choose a unique username

--- pages/landing.js
+++ pages/landing.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Image from "next/image";
-import { auth } from "../lib/firebase";
+import { auth, firestore } from "../lib/firebase";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import Button from "../components/Button";
 import { useContext } from "react";
 import { UserContext } from "../lib/context";
+import { useState, useEffect } from "react";
+import debounce from "lodash.debounce";
+import { collection, doc, getDoc, writeBatch } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 export default function Landing(props) {
     const { user, username } = useContext(UserContext);
@@ -65,4 +69,125 @@ function SignOutButton() {
     );
 }
 
-function UsernameForm() {}
+// username form
+function UsernameForm() {
+    const [formValue, setFormValue] = useState("");
+    const [isValid, setIsValid] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const { user, username } = useContext(UserContext);
+
+    useEffect(() => {
+        checkUsername(formValue);
+    }, [formValue]);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const userDoc = doc(firestore, `users/${user.uid}`);
+        const usernameDoc = getDoc(`username/${formValue}`);
+
+        // Commit both docs together as a batch write.
+        const batch = writeBatch(firestore);
+        batch.set(userDoc, {
+            username: formValue,
+            photoURL: user.photoURL,
+            displayName: user.displayName,
+        });
+        batch.set(usernameDoc, { uid: user.id });
+
+        await batch.commit();
+    };
+
+    function UsernameMessage({ username, isValid, loading }) {
+        if (loading) {
+            return <p>Checking .....</p>;
+        } else if (isValid) {
+            return <p className=" text-green-500">{username} is available!</p>;
+        } else if (username && !isValid) {
+            return (
+                <p className=" text-red-500">
+                    {username} That username is taken!
+                </p>
+            );
+        } else {
+            return <p></p>;
+        }
+    }
+
+    const onChange = (e) => {
+        // Force form value typed in form to match correct format
+        const val = e.target.value.toLowerCase();
+        const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+        // Only set form value if length is < 3 OR it passes regex
+        if (val.length < 3) {
+            setFormValue(val);
+            setLoading(false);
+            setIsValid(false);
+        }
+
+        if (re.test(val)) {
+            setFormValue(val);
+            setLoading(true);
+            setIsValid(false);
+        }
+    };
+
+    // Hit the database for username match after each debounced change
+    // useCallback is required for debounce to work
+
+    // it will work only when the use stop typing for 500 ms before running the checkUsername function
+    const checkUsername = useCallback(
+        debounce(async (username) => {
+            if (username.length >= 3) {
+                const ref = doc(firestore, `username`, `${username}`);
+                const docSnap = await getDoc(ref);
+                console.log("Firesotre read executed!");
+                console.log(!docSnap.exists());
+                setIsValid(!docSnap.exists());
+                setLoading(false);
+            }
+        }, 500),
+        []
+    );
+
+    return (
+        !username && (
+            <section>
+                <h3>Choose Username</h3>
+                <form onSubmit={onSubmit}>
+                    <input
+                        name="username"
+                        placeholder="username"
+                        value={formValue}
+                        onChange={onChange}
+                        autoComplete="false"
+                    />
+
+                    <UsernameMessage
+                        username={formValue}
+                        isValid={isValid}
+                        loading={loading}
+                    />
+
+                    <button
+                        type="submit"
+                        className=" font-bold px-4 h-10 w-fit bg-black rounded-full text-white"
+                        disabled={!isValid}
+                    >
+                        Choose
+                    </button>
+                    <h3>Debug State</h3>
+                    <div>
+                        Username : {formValue}
+                        <br />
+                        Loading : {loading.toString()}
+                        <br />
+                        Username Valid: {isValid.toString()}
+                    </div>
+                </form>
+            </section>
+        )
+    );
+}
