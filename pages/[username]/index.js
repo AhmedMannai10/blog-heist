90a1e3cc010a0d87802d5407a39b678e4f17bed5
Ahmed Mannai <mannaiahmed7518@gmail.com>
2022-10-12 00:30:35 +0100

    createing json serializable funct for dates

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,34 +1,45 @@
-import React from 'react'
+import React from "react";
 import UserProfile from "../../components/UserProfile";
 import PostFeed from "../../components/PostFeed";
-import { getUserWithUsername } from '../../lib/firebase';
+import { getUserWithUsername } from "../../lib/firebase";
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
 
-export async function getServerSideProps({query}){
+export async function getServerSideProps({ query }) {
+    const { username } = query;
 
-  const {username } = query;
+    const userDoc = await getUserWithUsername(username);
 
-  const userDoc = await getUserWithUsername(username);
+    let user = null;
+    let posts = null;
 
+    if (userDoc) {
+        //JSON serializable data
+        let user = null;
+        let posts = null;
 
-  let user = null;
-  let posts = null;
+        if (userDoc) {
+            user = userDoc.data();
+            const postsQuery = query(
+                collection(userDoc.ref, "posts"),
+                where("published", "==", true),
+                orderBy("createdAt", "desc"),
+                limit(5)
+            );
 
-  if(userDoc ){
-    // working here
-  }
+            posts =  (await (await getDocs(postsQuery)).docs.map(postToJson))
+        }
+    }
 
-  return {
-    props: {user, posts}
-  }
+    return {
+        props: { user, posts },
+    };
 }
 
-export default function UserProfilePage({user, posts}) {
-  return (
-    <div >
-      <UserProfile user={user}/>
-      <PostFeed posts={posts}  />
-    </div>
-    
-
-  )
+export default function UserProfilePage({ user, posts }) {
+    return (
+        <div>
+            <UserProfile user={user} />
+            <PostFeed posts={posts} />
+        </div>
+    );
 }
